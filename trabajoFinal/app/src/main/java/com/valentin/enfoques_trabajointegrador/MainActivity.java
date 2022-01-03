b307cb0cf155a01f24e508105c8a806cb57f85ee
vvarchioni <v.varchioni@gmail.com>
2022-01-02 22:44:12 -0300

    sin terminar - choque con la API de personajes

--- trabajoFinal/app/src/main/java/com/valentin/enfoques_trabajointegrador/MainActivity.java
+++ trabajoFinal/app/src/main/java/com/valentin/enfoques_trabajointegrador/MainActivity.java
@@ -3,12 +3,64 @@ package com.valentin.enfoques_trabajointegrador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnBuscar;
+    EditText txtBuscador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtBuscador = findViewById(R.id.txt_buscador);
+        btnBuscar = findViewById(R.id.btn_buscar);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buscarPersonaje(txtBuscador.getText());
+            }
+        });
+    }
+
+
+
+    private void buscarPersonaje (final Editable id) {
+        String url = "https://superheroapi.com/api/4764196856980399/" + id;
+
+        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    Toast.makeText(MainActivity.this, "RESULTADO PETICION = " + jsonObject.getString("name"), Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(postResquest);
     }
 }
